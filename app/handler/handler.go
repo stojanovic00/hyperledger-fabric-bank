3705986bba7fb4a5c1ab0ffc514d1ccaef25403b
Aleksandar <al.stojanovic0@gmail.com>
2024-02-07 19:34:32 +0100

    Added role authorization in JWT, init ledger inside network.sh

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"app/dto"
 	jwtUtil "app/jwt"
+	"app/model"
 	"app/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
@@ -11,7 +12,7 @@ import (
 )
 
 type Handler struct {
-	Users      map[string]string
+	Users      map[string]model.UserInfo
 	ChainCodes map[string]string
 }
 
@@ -22,7 +23,19 @@ func (h *Handler) Login(ctx *gin.Context) {
 		return
 	}
 
-	token, err := jwtUtil.GenerateJWT(userID)
+	userInfo, exists := h.Users[userID]
+	if !exists {
+		ctx.JSON(404, gin.H{"error": "user not found"})
+		return
+	}
+	var role string
+	if userInfo.Admin {
+		role = "ADMIN"
+	} else {
+		role = "USER"
+	}
+
+	token, err := jwtUtil.GenerateJWT(userInfo.UserId, role)
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": "Failed to generate JWT token"})
 		return
@@ -39,7 +52,7 @@ func (h *Handler) InitLedger(ctx *gin.Context) {
 	}
 	userId := userIdEntry.(string)
 
-	userOrg := h.Users[userId]
+	userInfo := h.Users[userId]
 
 	channel := ctx.Param("channel")
 	if channel == "" {
@@ -49,13 +62,13 @@ func (h *Handler) InitLedger(ctx *gin.Context) {
 
 	chaincodeId := h.ChainCodes[channel]
 
-	wallet, err := utils.CreateWallet(userId, userOrg)
+	wallet, err := utils.CreateWallet(userId, userInfo.Organization)
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": "Failed to create or populate wallet"})
 		return
 	}
 
-	gw, err := utils.ConnectToGateway(wallet, userOrg)
+	gw, err := utils.ConnectToGateway(wallet, userInfo.Organization)
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": "Failed to connect to gateway"})
 		return
@@ -84,6 +97,16 @@ func (h *Handler) submitTransaction(contract *gateway.Contract, transaction stri
 }
 
 func (h *Handler) AddUser(ctx *gin.Context) {
+	//Admins info
+	userIdEntry, ok := ctx.Get("userId")
+	if !ok {
+		ctx.JSON(400, gin.H{"error": "no auth parameters provided"})
+		return
+	}
+	adminId := userIdEntry.(string)
+	adminUserInfo := h.Users[adminId]
+
+	//Chaincode info
 	var user dto.User
 	if err := ctx.ShouldBindJSON(&user); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "couldn't resolve body"})
@@ -97,16 +120,13 @@ func (h *Handler) AddUser(ctx *gin.Context) {
 	}
 	chaincodeId := h.ChainCodes[channel]
 
-	userId := "u1"
-	userOrg := h.Users[userId]
-
-	wallet, err := utils.CreateWallet(userId, userOrg)
+	wallet, err := utils.CreateWallet(adminId, adminUserInfo.Organization)
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": "Failed to create or populate wallet"})
 		return
 	}
 
-	gw, err := utils.ConnectToGateway(wallet, userOrg)
+	gw, err := utils.ConnectToGateway(wallet, adminUserInfo.Organization)
 	if err != nil {
 		ctx.JSON(500, gin.H{"error": "Failed to connect to gateway"})
 		return
@@ -126,6 +146,14 @@ func (h *Handler) AddUser(ctx *gin.Context) {
 		ctx.JSON(400, gin.H{"error": "user already exists"})
 		return
 	}
+
+	//Register it also in SDK app
+	newUserInfo := model.UserInfo{
+		UserId:       user.Id,
+		Organization: adminUserInfo.Organization,
+		Admin:        false,
+	}
+	h.Users[user.Id] = newUserInfo
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "added user"})
 }
--- infrastructure/utils/init_ledger.sh
+++ infrastructure/utils/init_ledger.sh
@@ -1,10 +1,32 @@
+cd ..
+export PATH=${PWD}/bin:$PATH
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7050
+export FABRIC_CFG_PATH=$PWD/config/
+
 peer chaincode invoke \
   -o localhost:7000 --ordererTLSHostnameOverride orderer.example.com \
-  --tls --cafile "${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
+  --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
   -C channel1 \
   -n bankchaincode1  \
-  --peerAddresses localhost:7050 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
-  --peerAddresses localhost:8050 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
-  --peerAddresses localhost:9050 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
-  --peerAddresses localhost:10050 --tlsRootCertFiles "${PWD}/../organizations/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt" \
+  --peerAddresses localhost:7050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+  --peerAddresses localhost:8050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+  --peerAddresses localhost:9050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
+  --peerAddresses localhost:10050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt" \
   -c '{"function":"InitLedger","Args":[]}'
+
+
+#peer chaincode invoke \
+#  -o localhost:7000 --ordererTLSHostnameOverride orderer.example.com \
+#  --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" \
+#  -C channel2 \
+#  -n bankchaincode2  \
+#  --peerAddresses localhost:7050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" \
+#  --peerAddresses localhost:8050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" \
+#  --peerAddresses localhost:9050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt" \
+#  --peerAddresses localhost:10050 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt" \
+#  -c '{"function":"InitLedger","Args":[]}'
+
